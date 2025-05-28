007daabd1ce4da9059fb71353fd48a7fd3e34313
freetolga <freetolga@protonmail.com>
2025-05-28 23:14:22 +0300

    fully from scratch

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+vim.o.number = true
 vim.o.cp = false
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
@@ -12,176 +13,67 @@ vim.o.writebackup = false
 vim.o.swapfile = false
 vim.o.hidden = false
 vim.o.autoread = true
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46_cache/"
 vim.g.have_nerd_font = true
-vim.o.number = true
 vim.o.mouse = 'a'
 vim.o.showmode = false
-vim.schedule(function()
-  vim.o.clipboard = 'unnamedplus'
-end)
 vim.o.undofile = false
 vim.o.ignorecase = true
 vim.o.smartcase = true
-vim.o.signcolumn = 'yes'
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
-vim.o.splitright = true
-vim.o.splitbelow = true
-vim.o.inccommand = 'split'
 vim.o.cursorline = true
-vim.o.confirm = true
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
-end
+local Plug = vim.fn['plug#']
+vim.call('plug#begin')
 
----@type vim.Option
-local rtp = vim.opt.rtp
-rtp:prepend(lazypath)
+Plug('nvim-lua/plenary.nvim')
+Plug('nvchad/base46')
+Plug('nvchad/ui')
+Plug('nvim-treesitter/nvim-treesitter')
+Plug('neovim/nvim-lspconfig')
+Plug('saghen/blink.cmp', {['tag'] = 'v1.*'}) Plug('stevearc/conform.nvim')
 
-require('lazy').setup({
-  { 
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
-
-
- "nvim-lua/plenary.nvim",
--- { "nvim-tree/nvim-web-devicons", lazy = true },
- {
-   "nvchad/ui",
-    config = function()
-      require "nvchad" 
-    end
- },
-
- {
-    "nvchad/base46",
-    lazy = false,
-    build = function()
-      require("base46").load_all_highlights()
-    end,
-    theme = "solarized_dark",
- },
- { -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    lazy = false,
-    build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-    opts = {
-      highlight = {
-        enable = true,
-      },
-    },
-  },
-'neovim/nvim-lspconfig',
-
-{
-    'saghen/blink.cmp',
-    version = '1.*',
-    event = 'VimEnter',
-    lazy = false,
-    build = 'cargo build --release',
---- @module 'blink.cmp'
---- @type blink.cmp.Config
-    opts = {
-      keymap = {
-        preset = 'default',
-          ['<C-k>'] = { 'select_prev', 'fallback' },
-          ['<C-j>'] = { 'select_next', 'fallback' },
-      },
-      appearance = {
-        nerd_font_variant = 'normal',
-      },
-
-      completion = {
-        documentation = { auto_show = false, auto_show_delay_ms = 500 },
-      },
-
-      sources = {
-        default = { 'lsp', 'path'},
-        providers = {},
-      },
-
-      fuzzy = { implementation = 'rust' },
-
-      signature = { enabled = false },
-    },
-},
-
-    { -- Autoformat
-    'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
-    keys = {
-      {
-        '<leader>f',
-        function()
-          require('conform').format { async = true, lsp_format = 'fallback' }
-        end,
-        mode = '',
-        desc = '[F]ormat buffer',
-      },
-    },
-    opts = {
-      notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          return nil
-        else
-          return {
-            timeout_ms = 500,
-            lsp_format = 'fallback',
-          }
-        end
-      end,
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        zig = { 'zig fmt' },
-        c = { 'clang-format' },
-        cxx = { 'clang-format' },
-        cpp = { 'clang-format' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
-      }
-}
-},
--- end of plugins
-})
+vim.call('plug#end')
 
  for _, v in ipairs(vim.fn.readdir(vim.g.base46_cache)) do
    dofile(vim.g.base46_cache .. v)
  end
+
+require("nvchad")
+require("base46")
+
+require('nvim-treesitter.configs').setup({
+  auto_install = false,
+  ignore_install = { "all" },
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+})
+
+
+require("conform").setup({
+  formatters_by_ft = {
+    lua = { "stylua" },
+    rust = { "rustfmt", lsp_format = "fallback" },
+    zig = {"zig fmt"},
+  },
+})
+blink_cmp_opts = {
+    keymap = { preset = 'default' },
+
+    appearance = {
+      nerd_font_variant = 'mono'
+    },
+
+    completion = { documentation = { auto_show = false } },
+
+    sources = {
+      default = { 'lsp', 'path', 'buffer' },
+    },
+
+    fuzzy = { implementation = "rust" }
+}
+require('blink.cmp').setup(blink_cmp_opts)
 
 vim.lsp.enable('zls', {
     capabilities = require('blink.cmp').get_lsp_capabilities()
@@ -190,3 +82,4 @@ vim.lsp.enable('zls', {
 vim.lsp.enable('clangd', {
     capabilities = require('blink.cmp').get_lsp_capabilities()
 })
+
